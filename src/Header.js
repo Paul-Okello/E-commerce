34ba2628ba6e464aef1682093d3ab5c300c5d92d
Paul Okello <paulokello273@gmail.com>
2020-09-15 19:55:18 +0300

    Basket functionality complete

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import useStateValue from "./StateProvider";
 
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="header">
       <Link to="/">
@@ -35,7 +37,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__OptionLineTwo header__basketCount">0</span>
+            <span className="header__OptionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
--- src/Product.js
+++ src/Product.js
@@ -5,6 +5,7 @@ import useStateValue from "./StateProvider";
 function Product({ id, title, image, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
 
+  console.log("Basket: ", basket);
   const addToBasket = () => {
     dispatch({
       type: "ADD_TO_BASKET",
