1b7229c8c15b4ad3a0d0338d30f715293b60da36
Paul Okello <paulokello273@gmail.com>
2020-09-15 13:47:34 +0300

    React currency  format installed

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-        className="header__logo"
-      />
+      <Link to="/">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+          className="header__logo"
+        />
+      </Link>
+
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -28,10 +32,12 @@ function Header() {
           <span className="header__OptionLineOne">Your</span>
           <span className="header__OptionLineTwo">Prime</span>
         </div>
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__OptionLineTwo header__basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__OptionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
