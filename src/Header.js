7735812172a4aa3d3549c9237911bb6e2c7d09e5
Paul Okello <paulokello273@gmail.com>
2020-09-15 11:06:08 +0300

    Header completed styling

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="header">
-            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-             alt=""
-             className="header__logo"
-             />
+  return (
+    <div className="header">
+      <img
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+        className="header__logo"
+      />
+      <div className="header__search">
+        <input type="text" className="header__search" />
+        {/* Logo */}
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__OptionLineOne">Hello Guest</span>
+          <span className="header__OptionLineTwo">Sign In</span>
         </div>
-    )
+        <div className="header__option">
+          <span className="header__OptionLineOne">Returns</span>
+          <span className="header__OptionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__OptionLineOne">Your</span>
+          <span className="header__OptionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
