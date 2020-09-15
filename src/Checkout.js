94f9f2269c71dae64f04ee786e64675384b4c664
Paul Okello <paulokello273@gmail.com>
2020-09-15 21:27:29 +0300

    Checkout basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import useStateValue from "./StateProvider";
 import Subtotal from "./Subtotal";
 function Checkout() {
+  const [{ basket }] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -12,12 +15,19 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          {/* BasketItem */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
         <Subtotal />
-        <h2>Subtotal</h2>
       </div>
     </div>
   );
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  return (
+    <div className="checkoutProduct">
+      <img src={image} alt="" className="checkoutProduct__image" />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>Ksh</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+        <button>Remove from Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
