e38b5aa99f9388adb0cab7b1e83aa4a3eefca2b6
Paul Okello <paulokello273@gmail.com>
2020-09-15 15:08:56 +0300

    Added reducer

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,11 +1,12 @@
-import { createContext, useContext, useReducer } from "react"
-
+import React, { createContext, useContext, useReducer } from "react";
+//create data layer
 export const StateContext = createContext();
+// Wrap our app and provide Data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={(useReducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
 
-export const StateProvider = ({ reducer, initialState, children}) => {
-    <StateContext.Provider value={useReducer, initialState}>
-        {children}
-    </StateContext.Provider>
-};
-
-export const useStateValue = () = useContext(StateContext);
+//Pull info from data layer
+export const useStateValue = () => useContext(StateContext);
