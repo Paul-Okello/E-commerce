733ab1597937db1d0b232c2b81241921b691bfaf
Paul Okello <paulokello273@gmail.com>
2020-09-15 19:44:16 +0300

    Context API dispatch method complete

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,10 +3,11 @@ import React, { createContext, useContext, useReducer } from "react";
 export const StateContext = createContext();
 // Wrap our app and provide Data layer
 export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={(useReducer, initialState)}>
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>
 );
 
 //Pull info from data layer
-export const useStateValue = () => useContext(StateContext);
+const useStateValue = () => useContext(StateContext);
+export default useStateValue;
