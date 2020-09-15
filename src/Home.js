9546b755be60e3224d3bd7b2f5d8347e1cf95fa8
Paul Okello <paulokello273@gmail.com>
2020-09-15 13:09:32 +0300

    Product style complete

--- src/Home.js
+++ src/Home.js
@@ -12,16 +12,57 @@ function Home() {
           className="home__image"
         />
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            id="54321"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/41+e3refnZL.jpg"
+            rating={4}
+          />
+          <Product
+            id="93145"
+            title="
+            Amazon Echo Dot (3rd Generation) - Heather Gray"
+            price={56.99}
+            image="https://target.scene7.com/is/image/Target/GUEST_818dbba2-32e3-4592-976a-07b02ab5b7f8?wid=488&hei=488&fmt=pjpeg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="23605"
+            title="
+            Amazon Echo Dot (3rd Generation) - Heather Gray"
+            price={56.99}
+            image="https://target.scene7.com/is/image/Target/GUEST_818dbba2-32e3-4592-976a-07b02ab5b7f8?wid=488&hei=488&fmt=pjpeg"
+            rating={5}
+          />
+          <Product
+            id="23105"
+            title="
+            Amazon Echo Dot (3rd Generation) - Heather Gray"
+            price={56.99}
+            image="https://target.scene7.com/is/image/Target/GUEST_818dbba2-32e3-4592-976a-07b02ab5b7f8?wid=488&hei=488&fmt=pjpeg"
+            rating={2}
+          />
+          <Product
+            id="28145"
+            title="
+            Amazon Echo Dot (3rd Generation) - Heather Gray"
+            price={56.99}
+            image="https://target.scene7.com/is/image/Target/GUEST_818dbba2-32e3-4592-976a-07b02ab5b7f8?wid=488&hei=488&fmt=pjpeg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id="63845"
+            title="
+            Samsung UA65RU7300KXKE - 65 - Curved 4K UHD Smart"
+            price={56.99}
+            image="https://images.yaoota.com/lPK-8zLahgaUrTqkQEfHo11cqpo=/trim/yaootaweb-production-ke/media/crawledproductimages/c8910abf33ff1f47b8eee7a0e7a119647eb03daa.jpg"
+            rating={3}
+          />
         </div>
       </div>
     </div>
