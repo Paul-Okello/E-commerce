4c8bf3b5b712378b09f0a6f46c6016a27f8749b4
Paul Okello <paulokello273@gmail.com>
2020-09-15 22:25:04 +0300

    login page

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
-  return <div className="login"></div>;
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+          className="login__logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form action="">
+          <h5>E-mail</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login__signInButton">Sign in</button>
+          <p>
+            By continuing, you agree to Amazon's Conditions of Use and Privacy
+            Notice.
+          </p>
+          <button className="login__registerButton">Create account</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
