cff00228dabb6823605863134a91d0bfeb3776fa
Paul Okello <paulokello273@gmail.com>
2020-09-15 10:37:02 +0300

    Project setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB3cDxMV44yJmk-i8ApeM4m89nXGhZ_YT4",
+    authDomain: "e-commerce-26858.firebaseapp.com",
+    databaseURL: "https://e-commerce-26858.firebaseio.com",
+    projectId: "e-commerce-26858",
+    storageBucket: "e-commerce-26858.appspot.com",
+    messagingSenderId: "980947460816",
+    appId: "1:980947460816:web:39909b0b48b0f11b8574d5",
+    measurementId: "G-DMRRECHHPV"
+};
