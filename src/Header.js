019daf10b0f680e6e5a3fbb3d228bf0c4c73be37
Paul Okello <paulokello273@gmail.com>
2020-09-15 10:50:25 +0300

    Created header component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+             alt=""
+             className="header__logo"
+             />
+        </div>
+    )
+}
+
+export default Header
