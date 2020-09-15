544908c60fd382e48e2afc57f060416396fb5bf4
Paul Okello <paulokello273@gmail.com>
2020-09-15 12:27:54 +0300

    More styling

--- src/Product.js
+++ src/Product.js
@@ -10,8 +10,15 @@ function Product() {
           <small>$</small>
           <strong>19.99</strong>
         </p>
-        <div className="product__rating"></div>
+        <div className="product__rating">
+          <p>⭐</p>
+        </div>
       </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
+      <button>Add to Basket</button>
     </div>
   );
 }
