cae293bbad77940bece2ed64539774744e282c76
Paul Okello <paulokello273@gmail.com>
2020-09-16 09:36:07 +0300

    Login page complete

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -16,15 +24,31 @@ function Login() {
         <h1>Sign in</h1>
         <form action="">
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign in</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign in
+          </button>
           <p>
             By continuing, you agree to Amazon's Conditions of Use and Privacy
             Notice.
           </p>
-          <button className="login__registerButton">Create account</button>
+          <button className="login__registerButton" onClick={register}>
+            Create account
+          </button>
         </form>
       </div>
     </div>
