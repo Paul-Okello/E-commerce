c3c346c09f03c7f885aa7a300055d73c12a4bbe8
Paul Okello <paulokello273@gmail.com>
2020-09-16 10:11:50 +0300

    Create account

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,29 @@
+import { auth } from "./firebase";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const signIn = (e) => {
     e.preventDefault();
   };
+
   const register = (e) => {
     e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log("auth: ", auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
+
   return (
     <div className="login">
       <Link to="/">
